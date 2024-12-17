a29c0ab0669d6f53e3efb4db5f2e7ecb3204b21a
Jakob Csörgei Gustavsson <soulrivers@gmail.com>
2024-12-17 21:23:07 +0100

    day04 part1

---
+++ day04/Program.fs
@@ -0,0 +1,88 @@
+﻿module day04
+
+open System.IO  
+open System.Text.RegularExpressions;
+
+type Grid = char list list
+type Point = int*int
+
+let lines = File.ReadAllLines("input") |> List.ofArray
+let lines2 = File.ReadAllLines("input2") |> List.ofArray
+
+let example = File.ReadAllLines("example") |> List.ofArray
+let example2 = File.ReadAllLines("example2") |> List.ofArray
+
+let readInput (lst: string list) = 
+    lst |> List.map Seq.toList
+
+let collecti clt list =
+    list
+    |> List.indexed                // Create (index, value) pairs
+    |> List.collect (fun (i, x) -> clt i x) // Apply the collector function
+
+
+let findX (lst: Grid) =
+    lst
+    |>  collecti (fun outer a -> 
+            List.fold (fun (inner, acc) e -> 
+                if e = 'X' then (inner+1, (outer,inner) :: acc) else (inner+1, acc)
+            ) (0, []) a |> snd
+        )
+
+let word = "XMAS";
+
+let nextLetter (currentLetter: char) = 
+    match word.IndexOf(currentLetter) with 
+    | i when i = word.Length - 1 -> None
+    | i -> Some word[i + 1]
+
+let getLetter (grid: Grid) (pos: Point) = grid[fst pos][snd pos]
+
+let rec searchDirection (list: Grid) point nextPosition letterToFind =
+    match nextPosition list point with
+    | Some pos -> match getLetter list pos with
+                    | c when c = letterToFind -> 
+                        match nextLetter letterToFind with
+                            | None -> true
+                            | Some letter -> searchDirection list pos nextPosition letter
+                    | _ -> false
+    | None -> false
+
+let pointExists (list: Grid) ((a,b): Point) =
+    match a >= 0 && a < list.Length && b >= 0 && b < list[a].Length with
+    | true -> Some (a,b)
+    | false -> None
+
+let directions  =
+    [
+        // fun (a,b) -> (a,b)
+        fun (a,b) -> (a,b+1)
+        fun (a,b) -> (a,b-1)
+        
+        fun (a,b) -> (a+1,b)
+        fun (a,b) -> (a+1,b+1)
+        fun (a,b) -> (a+1,b-1)
+        
+        fun (a,b) -> (a-1,b)
+        fun (a,b) -> (a-1,b+1)
+        fun (a,b) -> (a-1,b-1)
+    ]
+
+let searchAllDirections (list: Grid) (xStartPosition: Point) =
+    directions |> List.fold (fun acc dir -> acc + if (searchDirection list xStartPosition (fun grid (a,b) -> pointExists grid (dir (a,b))) 'M') then 1 else 0) 0 
+
+let solution1 input : int = 
+    let lst = readInput input
+    lst
+    |> findX 
+    |> List.fold (fun acc i -> acc + searchAllDirections lst i) 0
+
+printfn "%d ?= %d" (solution1 example) 4
+printfn "%d = %d" (solution1 example2) 18
+
+printfn "%d" (solution1 lines)
+
+// printfn "%d" solution2
+
+
+    
